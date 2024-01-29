6628a02d6ea4f520f436f518f85959ad32ede3f6
EliasGurerro12 <jesusguerreropw12@gmail.com>
2024-01-29 11:16:12 -0600

    Insertar

--- Concursos2.Forms/Form1.cs
+++ Concursos2.Forms/Form1.cs
@@ -16,5 +16,10 @@ namespace Concursos2.Forms
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
